bc8c0677dc3ebce0a1f33f3966eea2d7f30a43a8
alphaShash <shashidharmaregowd@gmail.com>
2021-01-20 09:36:20 +0100

    *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 export class AppComponent {
   title = 'Angular hello-world';
 
+  courses = [1,2];
   post = {
     title: "Title",
     isFavorite: true
