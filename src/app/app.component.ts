f45a14eb4908b9a7ee7499a06dd3315ca20fe19a
alphaShash <shashidharmaregowd@gmail.com>
2021-01-21 13:53:12 +0100

    ngFor and TrackBy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,17 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular hello-world';
+   courses;
 
-  viewMode = 'map'
-
-  courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-  ]
-
-  onAdd() {
-    this.courses.push({id:4, name:'course4' })
+  loadCourses() {
+    this.courses = [
+      {id: 1, name: 'course1'},
+      {id: 2, name: 'course2'},
+      {id: 3, name: 'course3'},
+    ]
   }
-
-  onRemove(course) {
-    let index =  this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-  }
-
-  onChange(course) {
-    course.name = 'UPDATED';
-  }
-
-  // courses = [1,2];
-  post = {
-    title: "Title",
-    isFavorite: true
-  }
-
-  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
-    console.log('changes: ', eventArgs);
+ 
+  trackCourse(index, course) {
+    return course ? course.id : undefined
   }
 }
